850b67c5770c30aac0d549018253094d6a06060e
knguyen <knguyen9328@gmail.com>
2022-09-05 20:59:19 -0700

    enable pulldown on PA9 so input does not float

--- source/Core/Src/main.c
+++ source/Core/Src/main.c
@@ -169,7 +169,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : PA9 */
   GPIO_InitStruct.Pin = GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA10 */
